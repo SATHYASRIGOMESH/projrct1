8ec8cddc419a61a90cb2786adbe025f5db1da386
Sathya <113794046+SATHYASRIGOMESH@users.noreply.github.com>
2022-09-17 23:39:20 -0700

    Add files via upload

---
+++ travel agency.py
@@ -0,0 +1,153 @@
+print("********** welcome to BILLI ISLAND TRAVEL AGENCY************")
+print("iternary 6 nights & 7 days")
+print("couple pakage INR 1,69,000/-")
+print("for 2 adults ")
+print("excluding flight tickets")
+print("Book your flight tickets atleast 1.5-2 months earlier to save!")
+print("family tour is available now free booking")
+
+def  Booking (email):
+    
+      print("your email is verified")
+      Booking("sathya@gmail.com")
+
+print("You are looking our summer offers")
+print("1.dubai 4 day->60k")
+print("2.maldiv 3 day->40k")
+print("3.goa 3 day 20k")
+print("4.ooty 2 day 10k")
+print("5.kodaikanal 2day 10k")
+print("6.maleysia 20 days 80k")
+print("7.USA 2 month 1.5 l")
+print("8.kashmir 2.5 month 2L")
+print("9.aakra 1 month 78k")
+print("10.delhi 1 month 50k")
+
+
+def dubai():
+    print("4 days travel")
+    print("you need to pay 60k")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+
+def maldiv():
+    print("3 day travel")
+    print("you need to pay 40k")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+
+def goa():
+    print("3 day travel")
+    print("you need to pay 20k")
+    print("passport is not manitory")
+    print("you must need a covid certificate")
+
+def ooty():
+    print("2 day travel ")
+    print("you need to pay 10k")
+    print("passport is not manitory")
+    print("you must need a covid certificate")
+
+
+def kodaikanal():
+    print("2 days travel")
+    print("you need to pay 10k")
+    print("passport is not manitory")
+    print("you must need a covid certificate")
+
+
+def maleysia():
+    print("20 days ")
+    print("you need to pay 80k")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+
+
+def USA():
+    print("2 month travel")
+    print("you need to pay 1.5L")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+
+
+def Kashmir():
+    print("2.5 month travel")
+    print("you need to pay 2L")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+    print("you have to book")
+
+def AAkra():
+    print("1 month travel")
+    print("you need to pay 78k")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+    print("you have to book")
+
+
+def Delhi():
+    print("1 month travel")
+    print("you need to pay 50k")
+    print("passport is manitory")
+    print("you must need a covid certificate")
+    print("you have to book ")
+
+
+user=int(input("Enter your number:"))
+
+
+if user==1:
+
+   print("you are choosing dubai")
+   dubai()
+
+elif user==2:
+  print("you are choosing maldiv")
+
+  maldiv()
+
+elif user==3:
+
+    print("you are choosing goa")
+    goa()
+
+elif user==4:
+    print("you are choosing ooty")
+    ooty()
+
+elif user==5:
+    print("you are choosing kodaikanal")
+    kodaikanal()
+
+elif user==6:
+    print("you are choosing maleysia")
+    maleysia()
+
+elif user==7:
+    print("you are choosing USA")
+    USA()
+elif user==8:
+
+    print("you are choosing Kashmir")
+    Kashmir()
+
+elif user==9:
+
+    print("you are choosing AAkra")
+    AAkra()
+
+elif user==10:
+    print("you are choosing Delhi")
+
+    Delhi()
+    print("******Enjoy your journey*****")
+    
+else:
+
+    print("sorry numbers only available")
+
+    print("please enter here in there numbers only")
+    
+    print("please check your input")
+
+
